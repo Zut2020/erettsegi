f418f2e3a74c3ff7d309b5a546d8711fa10ce3b9
Zut2020 <pompos.balazs@gmail.com>
2015-06-15 15:45:45 +0200

    created beolvas function

--- 2015maj/2015maj/Program.cs
+++ 2015maj/2015maj/Program.cs
@@ -3,13 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _2015maj
 {
     class Program
     {
+        public const string fajlhely = @"C:\érettségi megoldas\forrasok\veetel.txt";
+
+        public struct adas
+        {
+            public int amator;
+            public int nap;
+            public string uzenet;
+        }
+
         static void Main(string[] args)
         {
+            List<adas> adasok = beolvas();
+            Console.ReadKey();
+        }
+
+        static List<adas> beolvas()
+        {
+            
+            List<adas> adasok = new List<adas>();
+            FileStream f = new FileStream(fajlhely, FileMode.Open);
+            StreamReader sr = new StreamReader(f);
+            string s = sr.ReadLine();
+            string[] stomb;
+            int i = 0;
+            adas sadas = new adas();
+            while (s != null)
+            {
+                stomb = s.Split(' ');
+                sadas.nap = int.Parse(stomb[0]);
+                sadas.amator = int.Parse(stomb[1]);
+
+                s = sr.ReadLine();
+                sadas.uzenet = s;
+                s = sr.ReadLine();
+                i++;
+                adasok.Add(sadas);
+            }
+            return adasok;
         }
     }
 }
